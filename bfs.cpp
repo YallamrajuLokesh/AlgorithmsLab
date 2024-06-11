5745d28e06ace153d7fd1bcc977553d4a78dee00
YallamrajuLokesh <149151515+YallamrajuLokesh@users.noreply.github.com>
2024-06-11 21:41:13 +0530

    Update bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -1,54 +1,52 @@
-#include <iostream>
-#include <vector>
-#include <map>
+#include<iostream>
+#include<queue>
+#include<list>
+#include<map>
 using namespace std;
 
-void dfsUtil(int Node, vector<int> adj[], map<int, bool>& visited, vector<int>& ls) {
-    visited[Node] = true;
-    ls.push_back(Node);
-    for (auto it : adj[Node]) {
-        if (!visited[it]) {
-            dfsUtil(it, adj, visited, ls);
-        }
+
+
+void bfs(int node,vector<int>adj[]){
+  cout<<"enter start"<<endl;
+  int start;
+  cin>>start;
+  map<int,bool>visited;
+  queue<int> q;
+  
+  vector<int>ans;
+  visited[node]=true;
+  q.push(node);
+  while(!q.empty()){
+    int current=q.front();
+    q.pop();
+    for(auto it:adj[current]){
+      if(!visited[it]){
+        visited[it]=true;
+        q.push(it);
+      }
     }
+  }
+  for(auto i:ans){
+    cout<<i<<" ";
+  }
+  cout<<endl;
 }
 
-void dfs(int V, vector<int> adj[]) {
-    int start;
-    cout << "Enter the starting vertex: ";
-    cin >> start;
 
-    map<int, bool> visited;  // Map to store visited status for each node
-    vector<int> ls;  // Vector to store the DFS traversal result
+int main(){
+int V,e;
+  cout<<"enter v"<<endl;
+  cin>>V;
+  cout<<"Enter e"<<endl;
+  cin>>e;
+  int u,v;
+  vector<int>adj[V];
 
-    dfsUtil(start, adj, visited, ls);  // Start DFS from the given starting node
-
-    // Print the DFS traversal result
-    cout << "DFS Traversal starting from vertex " << start << ": ";
-    for (auto i : ls) {
-        cout << i << " ";
-    }
-    cout << endl;
-}
-
-int main() {
-    int V, E;
-    cout << "Enter the number of vertices: ";
-    cin >> V;
-    cout << "Enter the number of edges: ";
-    cin >> E;
-
-    vector<int> adj[V];  // Create an adjacency list
-
-    cout << "Enter the edges (u v) one by one:\n";
-    for (int i = 0; i < E; i++) {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);  // Assuming the graph is undirected
-    }
-
-    dfs(V, adj);  // Perform DFS starting from the given vertex
-
-    return 0;
+  for(int i=0;i<e;i++){
+    cin>>u>>v;
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+    
+  }
+  bfs(V,adj);
 }
