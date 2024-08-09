7f44e716468bc4172fa3dee238c597a1831af796
YallamrajuLokesh <149151515+YallamrajuLokesh@users.noreply.github.com>
2024-08-09 17:13:39 +0530

    Update bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -4,49 +4,53 @@
 #include<map>
 using namespace std;
 
+void bfs(int start, vector<int> adj[]) {
+    map<int, bool> visited;
+    queue<int> q;
+    vector<int> ans;
+    
+    visited[start] = true;
+    q.push(start);
 
+    while (!q.empty()) {
+        int current = q.front();
+        q.pop();
+        ans.push_back(current);  // Add the node to the result
 
-void bfs(int node,vector<int>adj[]){
-  cout<<"enter start"<<endl;
-  int start;
-  cin>>start;
-  map<int,bool>visited;
-  queue<int> q;
-  
-  vector<int>ans;
-  visited[node]=true;
-  q.push(node);
-  while(!q.empty()){
-    int current=q.front();
-    q.pop();
-    for(auto it:adj[current]){
-      if(!visited[it]){
-        visited[it]=true;
-        q.push(it);
-      }
+        for (auto it : adj[current]) {
+            if (!visited[it]) {
+                visited[it] = true;
+                q.push(it);
+            }
+        }
     }
-  }
-  for(auto i:ans){
-    cout<<i<<" ";
-  }
-  cout<<endl;
+
+    for (auto i : ans) {
+        cout << i << " ";
+    }
+    cout << endl;
 }
 
-
-int main(){
-int V,e;
-  cout<<"enter v"<<endl;
-  cin>>V;
-  cout<<"Enter e"<<endl;
-  cin>>e;
-  int u,v;
-  vector<int>adj[V];
-
-  for(int i=0;i<e;i++){
-    cin>>u>>v;
-    adj[u].push_back(v);
-    adj[v].push_back(u);
+int main() {
+    int V, e;
+    cout << "Enter the number of vertices (V):" << endl;
+    cin >> V;
+    cout << "Enter the number of edges (E):" << endl;
+    cin >> e;
     
-  }
-  bfs(V,adj);
+    vector<int> adj[V];
+    int u, v;
+
+    cout << "Enter the edges (u v):" << endl;
+    for (int i = 0; i < e; i++) {
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    int start;
+    cout << "Enter the starting node for BFS:" << endl;
+    cin >> start;
+    
+    bfs(start, adj);
 }
